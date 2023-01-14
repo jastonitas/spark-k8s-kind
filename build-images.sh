27d59b67c1bdb280a4c38db3771890297f63a3b0
Jean Paul Astonitas <jastonitas@gmail.com>
2023-01-14 17:43:12 -0500

    removing extra images

--- build-images.sh
+++ build-images.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 docker build -t local-data/spark-store:spark-3.2.1 spark-store/.
 docker build -t local-data/spark-master:spark-3.2.1 spark-master/.
-docker build -t local-data/spark-slave:spark-3.2.1 spark-slave/.
+docker build -t local-data/spark-worker:spark-3.2.1 spark-worker/.
 docker build -t local-data/spark-submit:spark-3.2.1 spark-submit/.
-docker build -t local-data/spark-cron:spark-3.2.1 spark-cron/.
 
-kind load docker-image local-data/spark-store:spark-3.2.1 --name=data-cluster
-kind load docker-image local-data/spark-master:spark-3.2.1 --name=data-cluster
-kind load docker-image local-data/spark-slave:spark-3.2.1 --name=data-cluster
-kind load docker-image local-data/spark-submit:spark-3.2.1 --name=data-cluster
-kind load docker-image local-data/spark-cron:spark-3.2.1 --name=data-cluster
+kind load docker-image local-data/spark-store:spark-3.2.1 --name=datahub-clsr
+kind load docker-image local-data/spark-master:spark-3.2.1 --name=datahub-clsr
+kind load docker-image local-data/spark-worker:spark-3.2.1 --name=datahub-clsr
+kind load docker-image local-data/spark-submit:spark-3.2.1 --name=datahub-clsr
+
